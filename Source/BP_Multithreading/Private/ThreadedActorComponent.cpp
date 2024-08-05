bb2714d3a5b81d5b9fd1888e18c1161d361bd0f2
Andrii Loborchuk <loborchuka@gmail.com>
2024-08-05 18:43:35 +0300

    Added atomic container, queue

--- Source/BP_Multithreading/Private/ThreadedActorComponent.cpp
+++ Source/BP_Multithreading/Private/ThreadedActorComponent.cpp
@@ -15,20 +15,6 @@ UThreadedActorComponent::UThreadedActorComponent()
 
 }
 
-
-bool UThreadedActorComponent::RemoveAtomic(FName Identifier)
-{
-	//Already doesn't exist?
-	if (!AtomicMap.Contains(Identifier)) return false;
-
-		
-	delete AtomicMap[Identifier];
-	AtomicMap.Remove(Identifier);
-	AtomicMapTypes.Remove(Identifier);
-			
-	return true;
-}
-
 void UThreadedActorComponent::BeginPlay()
 {
 	Super::BeginPlay();
@@ -48,15 +34,6 @@ void UThreadedActorComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 	ThreadLogic->TickEvent->Trigger();
 	Thread->Kill();
 	delete ThreadLogic;
-
-	// We need to delete this data to clear the memory because the component has completed its lifecycle.
-	// This deletion is necessary because we created it with new.
-	TArray<FName> Keys;
-	AtomicMap.GetKeys(Keys);
-	for (auto Key : Keys)
-	{
-		delete AtomicMap[Key];
-	}
 }
 
 void UThreadedActorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -64,45 +41,4 @@ void UThreadedActorComponent::TickComponent(float DeltaTime, ELevelTick TickType
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	ThreadLogic->TickEvent->Trigger();
-}
-
-template <typename Type>
-void UThreadedActorComponent::SetAtomic(FName Identifier, Type Value)
-{
-	FString CurrentTypeName = typeid(Type).name();
-		
-	//Identifier exists?
-	if (AtomicMap.Contains(Identifier))
-	{
-		//Update identifier
-		AtomicMap[Identifier]->store(Value);
-		AtomicMapTypes[Identifier] = CurrentTypeName;
-
-		return;
-	}
-
-
-	//Can only reach here if owner or identifier didn't exist
-	AtomicMap.Add(Identifier, new std::atomic(std::variant<VariableTypes>(Value)));
-	AtomicMapTypes.Add(Identifier, CurrentTypeName);
-}
-
-template <typename Type>
-bool UThreadedActorComponent::GetAtomic(FName Identifier, Type& Value)
-{
-	//Doesn't exist? Exit
-	if (!AtomicMap.Contains(Identifier)) return false;
-
-		
-	//Different type?
-	FString PassedType = typeid(Type).name();
-	if (AtomicMapTypes[Identifier] != PassedType)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Tried to access atomic \"%ls\" with type \"%ls\" when it's defined as \"%ls\""),
-												 *Identifier.ToString(),  *PassedType, *AtomicMapTypes[Identifier]);
-		return false;
-	}
-			
-	Value = std::get<Type>(AtomicMap[Identifier]->load());
-	return true;
 }
---
+++ Source/BP_Multithreading/Public/AtomicContainer.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AtomicContainer.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class BP_MULTITHREADING_API UAtomicContainer : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, meta=(CustomStructureParam = "Value"), Category = "Atomics|Set")
+	void SetAtomic(FName Identifier, UProperty* Value);
+
+	UFUNCTION(BlueprintPure, meta=(CustomStructureParam = "Value"), Category = "Atomics|Get")
+	bool GetAtomic(FName Identifier, UProperty*& Value);
+
+	UFUNCTION(BlueprintPure, meta=(CustomStructureParam = "Value"), Category = "Atomics|Get")
+	bool GetAtomicAndRemove(FName Identifier, UProperty*& Value);
+	
+	UFUNCTION(BlueprintCallable, Category="Atomics|Remove")
+	bool RemoveAtomic(FName Identifier);
+
+	UFUNCTION(BlueprintCallable, meta=(CustomStructureParam = "Value"), Category = "Atomics|Set")
+	void AddQueue(FName Identifier, UProperty* Value);
+
+	UFUNCTION(BlueprintPure, meta=(CustomStructureParam = "Value"), Category = "Atomics|Set")
+	bool GetQueue(FName Identifier, UProperty*& Value);
+
+	UFUNCTION(BlueprintCallable, meta=(CustomStructureParam = "Value"), Category = "Atomics|Set")
+	void RemoveQueue(FName Identifier);
+	
+	virtual void BeginDestroy() override;
+
+private:
+	TMap<FName, std::atomic<UProperty*>*> AtomicMap;
+	TMap<FName, TQueue<UProperty*>*> QueueMap;
+};
